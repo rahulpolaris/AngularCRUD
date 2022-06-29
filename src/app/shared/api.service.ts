719585b01cdcafeadb99d1ab43a2bf3344cf96fb
Rahul Kumar <rahulprofessionalacc@gmail.com>
2022-06-29 14:30:08 +0530

    initial commit

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  postEmployee(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=>{
+      return res
+    }))
+  }
+  putEmployee(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=>{
+      return res
+    }))
+  }
+  getEmployee(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=>{
+      return res
+    }))
+  }
+  deleteEmployee(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=>{
+      return res
+    }))
+  }
+}
